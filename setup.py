b29d90bca4f493e318dd45c9e086adbb1a236765
Virinas-code <alexcode228@gmail.com>
2021-07-20 14:58:15 +0000

    Missing packages

--- setup.py
+++ setup.py
@@ -17,6 +17,19 @@ def get_installed_packages():
             module_list.append(module_name)
     return module_list
 
+def get_missing_packages(installed_packages):
+    module_list = required_modules
+    module_list.append("jeu_video")
+    for module in installed_packages:
+        module_list.remove(module)
+    return module_list
+
+installed_packages = get_installed_packages()
+missing_packages = get_missing_packages(installed_packages)
+
 print("Bienvenue dans le programme d'installation de Jeu vidéo !")
+print()
 print("Paquets dèja installés :")
-print("   ", "\t".join(get_installed_packages()))
+print("   ", "\t".join(installed_packages))
+print("Paquets à installer :")
+print("   ", "\t".join(missing_packages))
