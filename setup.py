de9d765f11cf3c08776e30d6d0bc77d0dc9e84e8
Virinas-code <alexcode228@gmail.com>
2021-07-20 15:07:20 +0000

    Setup: Python version detection

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ Jeu vidéo - Installation.
 
 Installation du jeu vidéo.
 """
+import os
+import sys
 import pkgutil
 
 required_modules = ["pygame"]
@@ -24,6 +26,20 @@ def get_missing_packages(installed_packages):
         module_list.remove(module)
     return module_list
 
+def detect_python():
+    test = os.system("python3")
+    if test == 0:
+        return "python3"
+    test = os.system("py -3")
+    if test == 0:
+        return "py -3"
+    print()
+    print("/!\\ Votre installation Python est instrouvable ! Merci d'installer Python et de vérifier qu'il peut être lancé avec la commande 'python3'.")
+    sys.exit(1)
+
+def install():
+    print("Détection de l'installation python..", end=" ")
+
 installed_packages = get_installed_packages()
 missing_packages = get_missing_packages(installed_packages)
 
@@ -33,3 +49,16 @@ print("Paquets dèja installés :")
 print("   ", "\t".join(installed_packages))
 print("Paquets à installer :")
 print("   ", "\t".join(missing_packages))
+answer = input("Continuer ? [O/n] ")
+if not answer or answer.lower() in ("o", "oui"):
+    answer = True
+elif answer.lower() in ("n", "non"):
+    answer = False
+else:
+    print("Réponse invalide. Annulation.")
+    answer = False
+
+if not answer:
+    print("Installation annulée.")
+else:
+    install()
