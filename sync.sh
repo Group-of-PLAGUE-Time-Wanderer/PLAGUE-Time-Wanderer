44454a2fb9a5153167178b6962ead62f70cfac98
Virinas-code <alexcode228@gmail.com>
2021-07-21 12:48:16 +0000

    Create sync auto

---
+++ sync.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/sh
+echo "Syncing..."
+echo " Fetching..."
+git fetch
+echo " Done."
+echo " Pulling..."
+git pull
+echo " Done."
+echo " Commiting..."
+git commit -a -m "Sync"
+echo " Done."
+echo " Pushing..."
+git push
+echo " Done."
+echo "Done."
