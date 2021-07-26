79587558cdeb56edcefe303319f604c29ee60cde
tt_thoma <thomwill212@gmail.com>
2021-07-26 12:14:40 +0400

    Divers
    Déplacement de certains fichiers dans "requirements/", ajout d'un sprite barre de progression et d'une fonction pour centrer les objets.

--- jeu_video/game.py
+++ jeu_video/game.py
@@ -12,18 +12,26 @@ pygame.init()
 
 bgcolor = (52,51,67) #couleur de fond
 
-longueur = 1000
-largeur = 700 #ajout de tailles réglables à l'avenir
+scsize = (1000, 700) #ajout de tailles réglables à l'avenir
 
-window = pygame.display.set_mode((longueur, largeur))
+def calccenter(img):
+	global scsize
+	isl = img.get_size()
+	return (scsize[0]/2-isl[0]/2,scsize[1]/2-isl[1]/2) #calcul condensé du centre de l'écran soustrait aux centre de l'image pour arriver pile au... milieu
+
+window = pygame.display.set_mode(scsize)
 pygame.display.set_caption("PLAGUE: Time Wanderer")
 icon = pygame.image.load("images/icon.png")
 pygame.display.set_icon(icon)
 
 window.fill(bgcolor) #remplir uniformément
+
 splash = pygame.image.load("images/splash.png") #splash screen au démarrage
-tmppos = (longueur/2-300,largeur/2-261) #centrer l'image
-window.blit(splash, tmppos) #afficher le splash screen
+window.blit(splash, calccenter(splash)) #afficher le splash screen
+
+progress = pygame.image.load("images/Progress.png")
+window.blit(progress, calccenter(progress))
+
 pygame.display.update() #ne pas oublier de rafraîchir l'écran !
 
 main_loop = True
---
+++ jeu_video/physics.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Projet du jeu vidéo - Moteur de jeu
+Ce script rassemblera tous les éléments pour le fonctionnement du jeu
+"""
+
+import pygame
+from pygame.locals import *
+
+class physic:
+    pass
