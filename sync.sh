8257818c58dac6cab989a2f6f542c6183f26cec9
Virinas-code <alexcode228@gmail.com>
2021-07-26 15:19:53 +0200

    Git: Update sync script

--- sync.sh
+++ sync.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/sh
 echo "Syncing..."
 echo -n " Fetching... "
-git fetch > log.txt
+git fetch > log1.txt
 echo " Done."
 echo -n " Pulling..."
-git pull > log.txt
+git pull > log2.txt
 echo " Done."
 echo -n " Adding..."
-git add . > log.txt
+git add . > log3.txt
 echo " Done."
 echo -n " Commiting..."
-git commit -a -m "Sync" > log.txt
+git commit -a -m "Sync" > log4.txt
 echo " Done."
 echo -n " Pushing..."
-git push > log.txt
+git push &> log.txt
 echo " Done."
 echo "Done."
