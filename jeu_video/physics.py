6fbfe4eccef9292fc14ec199bb97d48f3f72cce2
Virinas-code <alexcode228@gmail.com>
2021-07-26 14:30:55 +0200

    Game: Physics

--- jeu_video/physics.py
+++ jeu_video/physics.py
@@ -1,13 +1,41 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
 """
-Projet du jeu vidéo - Moteur de jeu
+Projet du jeu vidéo - Moteur de jeu.
+
 Ce script rassemblera tous les éléments pour le fonctionnement du jeu
 """
-
 import pygame
 from pygame.locals import *
 
+pygame.init()
+
+
 class physic:
     pass
+
+
+class Window:
+    def __init__(self, width, height, title, icon):
+        self.width = width
+        self.height = height
+        self.window = pygame.display.set_mode((self.width, self.height))
+        self.title = title
+        self.icon = icon
+        pygame.display.set_icon(icon)
+        pygame.display.set_caption(self.title)
+
+    def refresh(self):
+        pygame.display.update()
+
+    def bgfill(self, color):
+        self.window.fill(color)
+        self.refresh()
+
+
+class Image:
+    def __init__(self, path):
+        self.image = pygame.image.load(path)
+
+    def get(self):
+        return self.image
