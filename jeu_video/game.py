dd0f9ab906423e07f4e1205904a24109012c17ca
Virinas-code <alexcode228@gmail.com>
2021-07-30 15:28:03 +0200

    Game: Fix bugs, lint and speed up

--- jeu_video/game.py
+++ jeu_video/game.py
@@ -20,21 +20,24 @@ sentry_sdk.init(
 )
 pygame.init()
 
-main_window = game_utils.Window(1000, 700, "PLAGUE: Time Wanderer", game_utils.Image("images/icon.png").get())
+main_window = game_utils.Window(
+	1000, 700, "PLAGUE: Time Wanderer", "images/icon.png")
 
-bgcolor = (52,51,67) #couleur de fond
+bgcolor = (52, 51, 67)  # couleur de fond
+
 
 def calccenter(img):
 	global main_window
 	scsize = (main_window.width, main_window.height)
 	isl = img.get().get_size()
-	return (scsize[0]/2-isl[0]/2,scsize[1]/2-isl[1]/2) #calcul condensé du centre de l'écran soustrait aux centre de l'image pour arriver pile au... milieu
+	# calcul condensé du centre de l'écran soustrait aux centre de l'image pour arriver pile au... milieu
+	return (scsize[0]/2-isl[0]/2, scsize[1]/2-isl[1]/2)
 
 
 main_window.bgfill(bgcolor)
 
-splash = game_utils.Image("images/splash.png") #splash screen au démarrage
-main_window.add_image(splash, (0, 0)) #afficher le splash screen
+splash = game_utils.Image("images/splash.png")  # splash screen au démarrage
+main_window.add_image(splash, (0, 0))  # afficher le splash screen
 
 progress = game_utils.Image("images/Progress.png")
 progressed = game_utils.Image("images/progressed.png")
--- jeu_video/game_utils.py
+++ jeu_video/game_utils.py
@@ -5,15 +5,18 @@ Projet du jeu vidéo - Moteur de jeu.
 
 Ce script rassemblera tous les éléments pour le fonctionnement du jeu
 """
+import time
 import pygame
 from pygame.locals import *
 
 pygame.init()
 
-def center_coords(self, coords, image):
+
+def center_coords(coords, image, window):
     image_size = image.get().get_size()
     image_center = (image_size[0] / 2, image_size[1] / 2)
-    centered_coords = (coords[0] + self.width / 2 - image_center[0], coords[1] + self.height / 2 - image_center[1])
+    centered_coords = (coords[0] + window.width / 2 - image_center[0],
+                       coords[1] + window.height / 2 - image_center[1])
     return centered_coords
 
 
@@ -27,19 +30,18 @@ class Window:
         self.height = height
         self.window = pygame.display.set_mode((self.width, self.height))
         self.title = title
-        self.icon = icon
+        self.icon = Image(icon).get()
         self.images = list()
         self.bg = None
-        pygame.display.set_icon(icon)
+        pygame.display.set_icon(self.icon)
         pygame.display.set_caption(self.title)
 
     def refresh(self):
-        pygame.display.update()
+        pygame.display.flip()
 
     def bgfill(self, color):
         self.bg = color
         self.window.fill(color)
-        self.refresh()
 
     def image_center(self, image):
         image_size = image.get().get_size()
@@ -48,23 +50,35 @@ class Window:
 
     def add_image(self, image, position):
         self.images.append((image, position))
-        self.window.blit(image.get(), center_coords(position, image))
-        self.refresh()
+        self.load_image(image, position)
+
+    def load_image(self, image, position):
+        self.window.blit(image.get(),
+                         center_coords(position, image, self))
 
     def main_loop(self):
         main_loop = True
+        print(self.images)
         while main_loop:
+            print("loop")
             for event in pygame.event.get():
                 if event.type == QUIT:
                     main_loop = False
             if self.bg:
                 self.bgfill(self.bg)
             for image in self.images:
-                self.add_image(image[0], image[1])
+                print(image[0])
+                self.load_image(image[0], image[1])
+            self.refresh()
+
 
 class Image:
     def __init__(self, path):
-        self.image = pygame.image.load(path)
+        self.path = path
+        self.image = pygame.image.load(path).convert_alpha()
 
     def get(self):
         return self.image
+
+    def __str__(self):
+        return "Image from " + self.path
