ef25dae4e87ff2bbc9025d5a0b104b51a10f4166
Virinas-code <alexcode228@gmail.com>
2021-09-05 19:32:00 +0200

    Plague 3: __init__.py

---
+++ plague3/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+PLAGUE: Time Wandere - Version 3.0
+
+Mix of precedent versions of Plague.
+####################################
+PLAYGROUUUUUUUUUUUUUUUUUUUUUUUUUUUUUND
+
+####################################
+"""
+from plague3.assets import assets
