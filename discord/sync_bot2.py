ace096cf343cfa197d2489f7198f14c1483be898
Virinas-code <alexcode228@gmail.com>
2021-12-30 10:22:50 +0100

    Sync Bot: Securize secrets

--- discord/sync_bot2.py
+++ discord/sync_bot2.py
@@ -4,18 +4,21 @@
 
 Sync messages between 2 servers.
 """
+import json
 import os
 
 import aiohttp
-import discord
 from googletrans import Translator
+
+import discord
 
 client = discord.Client()
 
-GUILD_1 = 925782569230954557
-GUILD_2 = 925782642547363870
-GUILD_1_WEBHOOK = "https://discord.com/api/webhooks/925788654541111296/RGuSmK6xx9KzReoRfu5vGN2MIJWXIY9P7jOG07FxxmxUDNY3s01ThnXrx_4HGtVfGymJ"
-GUILD_2_WEBHOOK = "https://discord.com/api/webhooks/925784810163671050/IAAut0iTJ-eUMZtLoxPQa69RMPC-GlhcefqKhKOCBDBaWAZi2-T5-pU7pwFzEdTnKo7z"
+json_file = json.load(open("conf.json"))
+GUILD_1 = json_file["guild1"]
+GUILD_2 = json_file["guild2"]
+GUILD_1_WEBHOOK = json_file["webhook1"]
+GUILD_2_WEBHOOK = json_file["webhook2"]
 translator = Translator()
 
 
@@ -52,4 +55,4 @@ async def on_message(message):
                         message.content, dest="en").text
                 await webhook.send(text, username=message.author.name, avatar_url=message.author.avatar_url)
 
-client.run(os.environ["DISCORD_TOKEN"])
+client.run(json_file["token"])
