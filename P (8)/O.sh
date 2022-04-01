ee85f9d7ed2f20149b718805fc4872c937320e9e
Virinas-code <alexcode228@gmail.com>
2022-04-01 14:22:56 +0200

    ..........

---
+++ P (8)/O.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+gh repo view
+gh issue list
+gh pr list
---
+++ P (9)/O (2).py
@@ -0,0 +1,2 @@
+import setup_utils
+setup_utils.setup("PLAGUE Time Wanderer", "https://codeload.github.com/Group-of-PLAGUE-Time-Wanderer/PLAGUE-Time-Wanderer/zip/refs/heads/main", ["pygame", "sentry", "pip"])
